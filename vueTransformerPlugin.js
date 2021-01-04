bc4d8582c37391bda0473d6d77289e4c28dd71f4
Thong Dang <thongdn.it@gmail.com>
2021-01-04 10:47:14 +0700

    add vue-native

---
+++ vueTransformerPlugin.js
@@ -0,0 +1,11 @@
+var upstreamTransformer = require('metro-react-native-babel-transformer');
+
+var vueNativeScripts = require('vue-native-scripts');
+var vueExtensions = ['vue'];
+
+module.exports.transform = function ({src, filename, options}) {
+  if (vueExtensions.some((ext) => filename.endsWith('.' + ext))) {
+    return vueNativeScripts.transform({src, filename, options});
+  }
+  return upstreamTransformer.transform({src, filename, options});
+};
